62a8ef98fd6ac5098ceaf634501a5abd5b219e4e
redjon42 <jbierce-wilson.f17@warren-wilson.edu>
2019-04-09 14:15:12 -0400

    ew12

---
+++ list_indexing.py
@@ -0,0 +1,13 @@
+"""
+winners= ['Jane', 'Bob', 'Naru', 'Simone']
+prizes = ['1st', '2nd', '3rd', '4th']
+write a segment of code to ask the user for a name, and see if the name is in the list of winners.  If it is, print the position plus one
+
+E.G.
+Name:  Jane
+Jane got 1st prize!
+or
+name:  Sam
+Sam did not get a prize.
+
+"""
